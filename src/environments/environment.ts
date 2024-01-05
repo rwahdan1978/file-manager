7c6c9237ca40f59f7a31d786d7564c17c011a1d8
rwahdan1978 <86670955+rwahdan1978@users.noreply.github.com>
2024-01-05 09:43:51 +0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     aws: {
-        accessKeyId: 'AKIAT4QNA4ENCJTRSZ22',
-        secretAccessKey: '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',
+        accessKeyId: '',
+        secretAccessKey: '',
     }
 }
