f6d87c26ceafcf15e2c54e16ec30880c205733c3
rwahdan1978 <ramiwahdan1978@gmail.com>
2024-01-07 02:47:29 +0400

    up2024

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
 export const environment = {
-    
+    aws: {
+        region: 'ap-south-1',
+        accessKeyId: process.env.accessKeyId,
+        secretAccessKey: process.env.secretAccessKey,
+    }
 }
