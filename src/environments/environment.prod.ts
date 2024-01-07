a7f7826e2ca76f7a9e7ef5e79ae0ac9fe8eef74d
rwahdan1978 <ramiwahdan1978@gmail.com>
2024-01-07 10:34:55 +0400

    updates 7-1-2024

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     aws: {
+        production: true,
         accessKeyId: process.env.accessKeyId,
         secretAccessKey: process.env.secretAccessKey,
         region: 'ap-south-1'
