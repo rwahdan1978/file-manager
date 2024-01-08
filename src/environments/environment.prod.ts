0ce02cfeca484c637587ce9a34377a5eeee6a1a4
rwahdan1978 <ramiwahdan1978@gmail.com>
2024-01-08 20:07:56 +0400

    others updates 2024

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     aws: {
         production: true,
-        accessKeyId: process.env.accessKeyId,
-        secretAccessKey: process.env.secretAccessKey,
-        region: 'ap-south-1'
+        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+        AWS_DEFAULT_REGION: process.env.AWS_DEFAULT_REGION
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment1 = {
     aws: {
-        accessKeyId: "AKIAT4QNA4ENJRYQU7VA",
-        secretAccessKey: "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",
-        region: 'ap-south-1'
+        AWS_ACCESS_KEY_ID: "AKIAT4QNA4ENJRYQU7VA",
+        AWS_SECRET_ACCESS_KEY: "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",
+        AWS_DEFAULT_REGION: 'ap-south-1'
     }
 }
