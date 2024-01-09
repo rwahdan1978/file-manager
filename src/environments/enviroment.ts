4cb55f8a7b34e9924a9eee118c0faddd27f6b043
rwahdan1978 <ramiwahdan1978@gmail.com>
2024-01-09 12:44:49 +0400

    last ok 9-1-2024

--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,6 +1,8 @@
 export const environments = {
 
 aws: {
-    
+    // AWS_ACCESS_KEY_ID: 'AKIAT4QNA4ENJRYQU7VA',
+    // AWS_SECRET_ACCESS_KEY: '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',
+    // AWS_DEFAULT_REGION: 'ap-south-1'
     }
 }
